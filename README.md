f9584ed9788c769ee5ee00805af825fb5c8cdcef
Mr. Toni Vanhala <toni.vanhala@vincit.fi>
2016-09-03 23:57:32 +0300

    Basic React project setup
    - Simple "Hello World" app
    - Webpack build with Babel to transpile ES6
    - Webpack dev server, React, React DOM
    - Bootstrap from CDN

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Training
+
+Content
+=======
+
+This repository contains a step-by-step React tutorial. 
+See branch `tutorial` for the actual steps.
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+var path_target = path.resolve(__dirname, 'dist');
+var path_node_modules = path.resolve(__dirname, 'node_modules');
+var path_app_src = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path_target,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js.?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+    ]
+  },
+  resolve: {
+    root: [path_app_src, path_node_modules],
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
